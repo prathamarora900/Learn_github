b69fc7a8e5cc963efa950105dab837ce7cd920d5
Pratham Arora <arorapratham901@gmail.com>
2021-06-01 21:49:44 +0530

    next commit

---
+++ namasta.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class namasta{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        System.out.println("The value you enter is x:"+ x)
+    }
+}
